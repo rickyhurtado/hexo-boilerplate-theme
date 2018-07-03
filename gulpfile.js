5154b6584a61703e249b795a103bb27edb85e657
Ricky Hurtado <odatruh.ykcir@gmail.com>
2018-07-03 23:49:45 +1200

    Add gulp js transpiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
-var cssPath = 'themes/boilerplate/source/css';
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sequence = require('gulp-sequence');
+
+var theme = 'themes/boilerplate';
+var cssPath = theme + '/source/css';
 var scssPath = cssPath + '/_scss';
+var scssFiles = scssPath + '/**/*.scss';
+var jsPath = theme + '/source/js';
+var jsFiles = jsPath + '/_lib/**/*.js';
 
 gulp.task('sass', function() {
   return gulp.src(scssPath + '/styles.scss')
@@ -11,13 +20,32 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(cssPath + '/'))
 });
 
-gulp.task('watch', function(){
-  gulp.watch(scssPath + '/**/*.scss', ['sass']);
-})
-
 gulp.task('minify-css', () => {
   return gulp.src(cssPath + '/styles.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(cssPath));
 });
+
+gulp.task('concat-scripts', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(jsPath))
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest(jsPath));
+});
+
+gulp.task('translate-es6', () =>
+  gulp.src(jsPath + '/app.min.js')
+    .pipe(babel({ presets: ['env'] }))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsPath))
+);
+
+gulp.task('compile-sass', sequence('sass', 'minify-css'));
+gulp.task('transpile-js', sequence('concat-scripts', 'translate-es6'));
+
+gulp.task('watch', function(){
+  gulp.watch(scssFiles, ['compile-sass']);
+  gulp.watch(jsFiles, ['transpile-js']);
+})
